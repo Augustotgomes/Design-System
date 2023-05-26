4f06350162a32171974534731169d31a818aed6b
Augusto Gomes <augusto.tgomes@gmail.com>
2023-05-26 19:44:04 -0300

    Criado componentes e stories books de Texto, Heading e Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/Augustotgomes.png',
+    alt: 'Augusto Gomes',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const Fallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
